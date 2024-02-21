6b7fe7fbd8395423dc4752aa75fb4a6289a02751
arnold buiuca <arnoldbuiuka@gmail.com>
2024-02-21 17:56:08 +0000

    set the enviroment for the database and heroku

--- baron/settings.py
+++ baron/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*rra2tc+f&aepxqoa91-$&+=#p+x89bqcbwhh4pc6!m(&8k4#b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
