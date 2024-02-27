3ca8d07747102da2f0bc9fd14778e595544821a4
arnold buiuca <arnoldbuiuka@gmail.com>
2024-02-27 18:53:22 +0000

    set the urls and views for the home app, and create a home page html

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 def home_page(request):
-    return HttpResponse("Hello, Blog!")
+    # Your view logic here
+    return render(request, 'home_page.html')
