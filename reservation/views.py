561ee80ac4a52d6e64a36bc7cd9923677b85a696
arnold buiuca <arnoldbuiuka@gmail.com>
2024-03-07 16:11:54 +0000

    move the reservation page behind the login and create confirmation messages for the reservations

--- reservation/views.py
+++ reservation/views.py
@@ -7,15 +7,14 @@ def book_reservation(request):
     """
     Renders the booking form and processes form submissions.
     """
-    reserve_form = ReservationForm()  
-
+    reserve_form = ReservationForm()
     if request.method == 'POST':
-        reserve_form = ReservationForm(request.POST)  
-        if reserve_form.is_valid(): 
-            reserve_form.save()
-            messages.success(request,"successfull reservation")  
-            
+        reserve_form = ReservationForm(request.POST)
+        if reserve_form.is_valid():
+            reservation = reserve_form.save(commit=False)
+            booking_details_message = f"Reservation confirmed for {reservation.date} at {reservation.time}."
+            reservation.save()
+            messages.success(request, booking_details_message)  
             return redirect('reservation') 
-
-    context = {'form': reserve_form}  
+    context = {'form': reserve_form}
     return render(request, "reservation/reservation.html", context)
