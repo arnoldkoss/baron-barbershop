05d6d48283eb4f313c4a9c973af7813816543cd2
arnold buiuca <arnoldbuiuka@gmail.com>
2024-03-09 13:14:17 +0000

    test comment submissions

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -3,12 +3,15 @@ from .forms import CommentForm
 
 
 class TestCommentForm(TestCase):
-
+    """
+    Test to ensure the form is considered valid when the body has content.
+    """
     def test_form_is_valid(self):
         comment_form = CommentForm({'body': 'This is a great post'})
         self.assertTrue(comment_form.is_valid())
-    
-
+    """
+    Test to ensure the form is considered invalid when the body is empty.
+    """
     def test_form_is_invalid(self):
         comment_form = CommentForm({'body': ''})
         self.assertFalse(comment_form.is_valid(), msg="Form is valid")
