553267e5e9978f16fc5e21161506cd09af41a1f4
arnold buiuca <arnoldbuiuka@gmail.com>
2024-03-11 17:03:39 +0000

    fix the tailwind errors nin the about page and take away summernote from the admin panel for the about page

--- about/admin.py
+++ about/admin.py
@@ -3,15 +3,5 @@ from django.contrib import admin
 from .models import About
 
 
-@admin.register(About)
-class AboutAdmin(SummernoteModelAdmin):
-    """
-    Custom admin class for About model.
 
-    This class specifies how the About model should
-     be represented in the Django admin interface.
-    It extends SummernoteModelAdmin to enable the
-     Summernote WYSIWYG editor for the 'content' field.
-    """
-
-    summernote_fields = ('content',)
+admin.site.register(About)
